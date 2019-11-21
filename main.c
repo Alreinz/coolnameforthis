6a1cbd36cde08ee87b53f55261dbcd4bc54eaae8
alreinz <lorenzdiz@gmail.com>
2019-11-21 17:34:25 +0800

    Added: login

--- main.c
+++ main.c
@@ -2,9 +2,17 @@
 #include <stdio.h>
 
 int main () {
-	printf("Hello World");
+	int password;
+	printf("Hello World\n");
 	
-	printf("This is another change");
+	printf("This is another change\n");
+
+	printf("Enter login password (password is 123)\n");
+	scanf("%d", &password);
+	
+	if ( password == 123 ) {
+		printf("Success");
+	}
 	
 	return 0;
 }
